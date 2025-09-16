30f7b02679ec4cc825f92979457409970f613fce
Munaf2112 <mmohamedabdulmunab@gmail.com>
2025-09-16 22:47:29 +0530

    Add files via upload

---
+++ pro2_Erc20.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract MyCustomToken is ERC20, Ownable {
+    constructor(uint256 initialSupply)
+        ERC20("MyCustomToken", "MCT")
+        Ownable(msg.sender)  // <-- Pass msg.sender to Ownable
+    {
+        _mint(msg.sender, initialSupply * (10 ** decimals()));
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
